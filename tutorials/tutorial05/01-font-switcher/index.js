27a9277d141a74135f76517d6c54f60b3a2c2804
Lena Huang <lenazyhuang@gmail.com>
2022-04-30 12:01:23 -0500

    tutorial 5

--- tutorials/tutorial05/01-font-switcher/index.js
+++ tutorials/tutorial05/01-font-switcher/index.js
@@ -1,25 +1,25 @@
 const makeBigger = () => {
-   // alert('make bigger!');
+   
    var header = document.querySelector('h1');
    var text = document.querySelector('div.content');
 
    style = window.getComputedStyle(header, text, null).getPropertyValue('font-size');
    currSize = parseFloat(style);
 
-   header.style.fontSize = (currSize + 10) + 'px';
-   text.style.fontSize = (currSize + 8) + 'px';
+   header.style.fontSize = (currSize + 6) + 'px';
+   text.style.fontSize = (currSize + 4) + 'px';
 };
 
 const makeSmaller = () => {
-   // alert('make smaller!');
+   
    var header = document.querySelector('h1');
    var text = document.querySelector('div.content');
 
    style = window.getComputedStyle(header, text, null).getPropertyValue('font-size');
    currSize = parseFloat(style);
    
-   header.style.fontSize = (currSize - 10) + 'px';
-   text.style.fontSize = (currSize - 12) + 'px';
+   header.style.fontSize = (currSize - 4) + 'px';
+   text.style.fontSize = (currSize - 6) + 'px';
 };
 
 document.querySelector("#a1").addEventListener('click', makeBigger);
