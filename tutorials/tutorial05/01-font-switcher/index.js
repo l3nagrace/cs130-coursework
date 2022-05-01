b6c3ab5f71cd6b15b2b040785cff28e8f6a8c124
Lena Huang <lenazyhuang@gmail.com>
2022-04-30 20:44:57 -0500

    .

--- tutorials/tutorial05/01-font-switcher/index.js
+++ tutorials/tutorial05/01-font-switcher/index.js
@@ -1,26 +1,26 @@
 const makeBigger = () => {
    
-   var header = document.querySelector('h1');
-   var text = document.querySelector('div.content');
+   var header = document.querySelector("h1");
+   var text = document.querySelector("div.content");
 
-   style = window.getComputedStyle(header, text, null).getPropertyValue('font-size');
+   style = window.getComputedStyle(header, text, null).getPropertyValue("font-size");
    currSize = parseFloat(style);
 
-   header.style.fontSize = (currSize + 6) + 'px';
-   text.style.fontSize = (currSize + 4) + 'px';
+   header.style.fontSize = (currSize + 6) + "px";
+   text.style.fontSize = (currSize + 4) + "px";
 };
 
 const makeSmaller = () => {
    
-   var header = document.querySelector('h1');
-   var text = document.querySelector('div.content');
+   var header = document.querySelector("h1");
+   var text = document.querySelector("div.content");
 
-   style = window.getComputedStyle(header, text, null).getPropertyValue('font-size');
+   style = window.getComputedStyle(header, text, null).getPropertyValue("font-size");
    currSize = parseFloat(style);
    
-   header.style.fontSize = (currSize - 4) + 'px';
-   text.style.fontSize = (currSize - 6) + 'px';
+   header.style.fontSize = (currSize - 4) + "px";
+   text.style.fontSize = (currSize - 6) + "px";
 };
 
-document.querySelector("#a1").addEventListener('click', makeBigger);
-document.querySelector("#a2").addEventListener('click', makeSmaller);
+document.querySelector("#a1").addEventListener("click", makeBigger);
+document.querySelector("#a2").addEventListener("click", makeSmaller);
