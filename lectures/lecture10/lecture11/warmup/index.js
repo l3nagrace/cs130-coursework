0b186e17b2e69b0b56fbab097f19136e3d6d1ca2
Lena Huang <lenazyhuang@gmail.com>
2022-05-06 18:43:49 -0500

    tutorial 6

---
+++ lectures/lecture10/lecture11/warmup/index.js
@@ -0,0 +1,15 @@
+const add_numbers = (ev) => {
+    // Your code here...
+    console.log('add numbers');
+    let num1 = document.querySelector('#num1').value;
+    console.log(num1);
+
+    let num2 = document.querySelector('#num2').value;
+    console.log(num2);
+
+    let result = Number(num1) + Number(num2);
+    console.log(result);
+
+    document.querySelector('#answer').innerHTML = result;
+}
+
