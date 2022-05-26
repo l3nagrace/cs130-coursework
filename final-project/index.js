41cb1c4cf5c770f864c7109bb8017b3c9fbcf687
Lena Huang <lenazyhuang@gmail.com>
2022-05-26 14:27:44 -0500

    update

--- final-project/index.js
+++ final-project/index.js
@@ -49,3 +49,14 @@ anime.timeline({loop: true})
     duration: 500,
     delay: 500
   });
+
+  var post= document.getElementById("post");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered").appendChild(li);
+ 
+});
